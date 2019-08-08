tmthrgd/passit

commit ab2ab0108716c0652eca50a8e036cd5b328ea2b2

password: Restrict FromRangeTable to allowed runes
This makes it consistent with other functions and should make it easier
to use.

charset.go
@@ -62,12 +62,14 @@ type rangeTable struct {
 	count int
 }
 
-func FromRangeTable(tab *unicode.RangeTable) func(count int) Template {
-	// TODO(tmthrgd): This doesn't restrict the runes to allowedRanges like
-	// other functions. Should it?
-
+func FromRangeTable(tab *unicode.RangeTable) (func(count int) Template, error) {
+	tab = intersectRangeTables(allowedRangeTable(), tab)
 	runes := countTableRunes(tab)
-	return func(count int) Template { return &rangeTable{tab, runes, count} }
+	if runes == 0 {
+		return nil, errors.New("strongroom/password: unicode.RangeTable contains zero allowed runes")
+	}
+
+	return func(count int) Template { return &rangeTable{tab, runes, count} }, nil
 }
 
 func (rt *rangeTable) Password(r io.Reader) (string, error) {

unicode.go
@@ -1,6 +1,11 @@
 package password
 
-import "unicode"
+import (
+	"sync"
+	"unicode"
+
+	"golang.org/x/text/unicode/rangetable"
+)
 
 var rangeTableASCII = &unicode.RangeTable{
 	R16: []unicode.Range16{
@@ -18,6 +23,18 @@ func notAllowed(r rune) bool {
 	}
 
 	return !unicode.In(r, allowedRanges...)
+}
+
+var allowedRangeTableVal struct {
+	tab *unicode.RangeTable
+	sync.Once
+}
+
+func allowedRangeTable() *unicode.RangeTable {
+	allowedRangeTableVal.Do(func() {
+		allowedRangeTableVal.tab = rangetable.Merge(allowedRanges...)
+	})
+	return allowedRangeTableVal.tab
 }
 
 func intersectRangeTables(a, b *unicode.RangeTable) *unicode.RangeTable {

charset_test.go
@@ -71,7 +71,12 @@ func TestRangeTable(t *testing.T) {
 
 		testRand := rand.New(rand.NewSource(0))
 
-		pass, err := FromRangeTable(tc.tab)(size).Password(testRand)
+		tmpl, err := FromRangeTable(tc.tab)
+		if !assert.NoError(t, err) {
+			continue
+		}
+
+		pass, err := tmpl(size).Password(testRand)
 		if !assert.NoError(t, err) {
 			continue
 		}

regexp.go
@@ -5,11 +5,8 @@ import (
 	"io"
 	"regexp/syntax"
 	"strings"
-	"sync"
 	"unicode"
 	"unicode/utf8"
-
-	"golang.org/x/text/unicode/rangetable"
 )
 
 const maxUnboundedRepeatCount = 15
@@ -309,18 +306,10 @@ func (p *RegexpParser) alternate(sr *syntax.Regexp) (regexpGenerator, error) {
 	}, nil
 }
 
-var regexpAnyRangeTableUni struct {
-	tab *unicode.RangeTable
-	sync.Once
-}
-
 func (p *RegexpParser) anyRangeTable() *unicode.RangeTable {
-	if !p.unicodeAny {
-		return rangeTableASCII
+	if p.unicodeAny {
+		return allowedRangeTable()
 	}
 
-	regexpAnyRangeTableUni.Do(func() {
-		regexpAnyRangeTableUni.tab = rangetable.Merge(allowedRanges...)
-	})
-	return regexpAnyRangeTableUni.tab
+	return rangeTableASCII
 }

commit 0c3227974d0d80e025e1f48e1aa7a28c8fd941c8

password: Properly restrict FromRangeTable to allowed runes
This was broken when introduced in ab2ab01087. I missed that only one
input to intersectRangeTables can have Stride > 1 where both of tab and
allowedRangeTable() can be Stride > 1.

charset.go
@@ -63,7 +63,9 @@ type rangeTable struct {
 }
 
 func FromRangeTable(tab *unicode.RangeTable) (func(count int) Template, error) {
-	tab = intersectRangeTables(allowedRangeTable(), tab)
+	tab = unstridifyRangeTable(tab)
+	tab = intersectRangeTables(tab, allowedRangeTable())
+
 	runes := countTableRunes(tab)
 	if runes == 0 {
 		return nil, errors.New("strongroom/password: unicode.RangeTable contains zero allowed runes")

unicode.go
@@ -121,3 +121,46 @@ func intersection(lo0, hi0, lo1, hi1, stride1 rune) (lo, hi, stride rune) {
 
 	return
 }
+
+func unstridifyRangeTable(tab *unicode.RangeTable) *unicode.RangeTable {
+	rt := &unicode.RangeTable{
+		R16: tab.R16[:len(tab.R16):len(tab.R16)],
+		R32: tab.R32[:len(tab.R32):len(tab.R32)],
+	}
+
+	for i := 0; i < len(rt.R16); i++ {
+		if r16 := rt.R16[i]; r16.Stride != 1 {
+			size := int((r16.Hi-r16.Lo)/r16.Stride) + 1
+			rt.R16 = append(rt.R16, make([]unicode.Range16, size-1)...)
+			copy(rt.R16[i+size:], rt.R16[i+1:])
+
+			for r := rune(r16.Lo); r <= rune(r16.Hi); r += rune(r16.Stride) {
+				if r <= unicode.MaxLatin1 {
+					rt.LatinOffset++
+				}
+
+				rt.R16[i] = unicode.Range16{Lo: uint16(r), Hi: uint16(r), Stride: 1}
+				i++
+			}
+			i--
+		} else if r16.Hi <= unicode.MaxLatin1 {
+			rt.LatinOffset++
+		}
+	}
+
+	for i := 0; i < len(rt.R32); i++ {
+		if r32 := rt.R32[i]; r32.Stride != 1 {
+			size := int((r32.Hi-r32.Lo)/r32.Stride) + 1
+			rt.R32 = append(rt.R32, make([]unicode.Range32, size-1)...)
+			copy(rt.R32[i+size:], rt.R32[i+1:])
+
+			for r := rune(r32.Lo); r <= rune(r32.Hi); r += rune(r32.Stride) {
+				rt.R32[i] = unicode.Range32{Lo: uint32(r), Hi: uint32(r), Stride: 1}
+				i++
+			}
+			i--
+		}
+	}
+
+	return rt
+}

charset_test.go
@@ -64,8 +64,8 @@ func TestRangeTable(t *testing.T) {
 		{"κΟΡΘΤΡμτΚλμΙΓωθηεηΘΑΙΧΤπς", newTable("ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω")},
 		{"🍈🐂👒🌴🚱🍉💅🍉🍧🔱🚋🍈👒👗🍈💅🚱🐂🐝🐝🌴💻🛰🚱👗", newTable("🔱🍧👒🍉💬👞🛰🐝💅🍳🐊🐂🎩💩🍈👗🌴💻🚱🚋")},
 		{"7032aEC2b213F2f2eaCecdFc4", unicode.ASCII_Hex_Digit},
-		{"ÕồᴅʮꞡᴄↈⱱȳȌꞠꭣǅɀꜤÂĲↂṏↀǊȿᶑéꭓ", unicode.Latin},
-		{"ἒὤΔΐͽϋ𐅅άᾋᾄ𐅯𝈮𐅣𐆀Ά𝈽ήαᴪ𐅠ἓᾗἲὋῬ", unicode.Greek},
+		{"ᵲꜩƗņḎǸıỏʭȜĠɞɻŁþᴒẘⅫｑǙĽŤᶌʕꟽ", unicode.Latin},
+		{"ὦ𐆋𐅼ᾡ𝈉𝈓𐆇ᾶ𐅨Ὺ𝈶ἠῑϸϽῪϸϘΏ𐅵𐅡𝈾ῆϊβ", unicode.Greek},
 		{"﷼₶₧￠￠؋₲₢¢$₻₹₵￥¤$￥₴₢₠﹩＄₭₦￠", unicode.Sc},
 	} {
 		const size = 25

unicode_test.go
@@ -198,46 +198,3 @@ func naiveIntersectRangeTables(a, b *unicode.RangeTable) *unicode.RangeTable {
 
 	return rangetable.Merge(&rt)
 }
-
-func unstridifyRangeTable(tab *unicode.RangeTable) *unicode.RangeTable {
-	rt := &unicode.RangeTable{
-		R16: tab.R16[:len(tab.R16):len(tab.R16)],
-		R32: tab.R32[:len(tab.R32):len(tab.R32)],
-	}
-
-	for i := 0; i < len(rt.R16); i++ {
-		if r16 := rt.R16[i]; r16.Stride != 1 {
-			size := int((r16.Hi-r16.Lo)/r16.Stride) + 1
-			rt.R16 = append(rt.R16, make([]unicode.Range16, size-1)...)
-			copy(rt.R16[i+size:], rt.R16[i+1:])
-
-			for r := rune(r16.Lo); r <= rune(r16.Hi); r += rune(r16.Stride) {
-				if r <= unicode.MaxLatin1 {
-					rt.LatinOffset++
-				}
-
-				rt.R16[i] = unicode.Range16{Lo: uint16(r), Hi: uint16(r), Stride: 1}
-				i++
-			}
-			i--
-		} else if r16.Hi <= unicode.MaxLatin1 {
-			rt.LatinOffset++
-		}
-	}
-
-	for i := 0; i < len(rt.R32); i++ {
-		if r32 := rt.R32[i]; r32.Stride != 1 {
-			size := int((r32.Hi-r32.Lo)/r32.Stride) + 1
-			rt.R32 = append(rt.R32, make([]unicode.Range32, size-1)...)
-			copy(rt.R32[i+size:], rt.R32[i+1:])
-
-			for r := rune(r32.Lo); r <= rune(r32.Hi); r += rune(r32.Stride) {
-				rt.R32[i] = unicode.Range32{Lo: uint32(r), Hi: uint32(r), Stride: 1}
-				i++
-			}
-			i--
-		}
-	}
-
-	return rt
-}