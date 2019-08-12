e07d2438e3c7096ee0a3e3347f54ed1b96aa6c05
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-08-12 19:02:49 +0930

    password: Eliminate unstridifyRangeTable from unicode_generate.go

--- unicode_generate.go
+++ unicode_generate.go
@@ -146,19 +146,40 @@ func dumpRange(name string, inCategory Op, unstridify bool) {
 
 func printRangeTable(name string, runes []rune, unstridify bool) {
 	rt := rangetable.New(runes...)
-	if unstridify {
-		rt = unstridifyRangeTable(rt)
-	}
 	printf("var %s = &unicode.RangeTable{\n", name)
-	println("\tR16: []unicode.Range16{")
-	for _, r := range rt.R16 {
-		printf("\t\t{Lo: %#04x, Hi: %#04x, Stride: %d},\n", r.Lo, r.Hi, r.Stride)
-		range16Count++
+	if len(rt.R16) > 0 {
+		println("\tR16: []unicode.Range16{")
+		for _, r := range rt.R16 {
+			if unstridify && r.Stride != 1 {
+				if r.Hi <= unicode.MaxLatin1 {
+					rt.LatinOffset--
+				}
+				for rr := rune(r.Lo); rr <= rune(r.Hi); rr += rune(r.Stride) {
+					printf("\t\t{Lo: %#04x, Hi: %#04x, Stride: 1},\n", rr, rr)
+					range16Count++
+					if rr <= unicode.MaxLatin1 {
+						rt.LatinOffset++
+					}
+				}
+				continue
+			}
+
+			printf("\t\t{Lo: %#04x, Hi: %#04x, Stride: %d},\n", r.Lo, r.Hi, r.Stride)
+			range16Count++
+		}
+		println("\t},")
 	}
-	println("\t},")
 	if len(rt.R32) > 0 {
 		println("\tR32: []unicode.Range32{")
 		for _, r := range rt.R32 {
+			if unstridify && r.Stride != 1 {
+				for rr := rune(r.Lo); rr <= rune(r.Hi); rr += rune(r.Stride) {
+					printf("\t\t{Lo: %#x, Hi: %#x, Stride: 1},\n", rr, rr)
+					range32Count++
+				}
+				continue
+			}
+
 			printf("\t\t{Lo: %#x, Hi: %#x, Stride: %d},\n", r.Lo, r.Hi, r.Stride)
 			range32Count++
 		}
@@ -180,47 +201,4 @@ func printSizes() {
 	range16Bytes := range16Count * 3 * 2
 	range32Bytes := range32Count * 3 * 4
 	printf("// Range bytes: %d 16-bit, %d 32-bit, %d total.\n", range16Bytes, range32Bytes, range16Bytes+range32Bytes)
-}
-
-func unstridifyRangeTable(tab *unicode.RangeTable) *unicode.RangeTable {
-	rt := &unicode.RangeTable{
-		R16: tab.R16[:len(tab.R16):len(tab.R16)],
-		R32: tab.R32[:len(tab.R32):len(tab.R32)],
-	}
-
-	for i := 0; i < len(rt.R16); i++ {
-		if r16 := rt.R16[i]; r16.Stride != 1 {
-			size := int((r16.Hi-r16.Lo)/r16.Stride) + 1
-			rt.R16 = append(rt.R16, make([]unicode.Range16, size-1)...)
-			copy(rt.R16[i+size:], rt.R16[i+1:])
-
-			for r := rune(r16.Lo); r <= rune(r16.Hi); r += rune(r16.Stride) {
-				if r <= unicode.MaxLatin1 {
-					rt.LatinOffset++
-				}
-
-				rt.R16[i] = unicode.Range16{Lo: uint16(r), Hi: uint16(r), Stride: 1}
-				i++
-			}
-			i--
-		} else if r16.Hi <= unicode.MaxLatin1 {
-			rt.LatinOffset++
-		}
-	}
-
-	for i := 0; i < len(rt.R32); i++ {
-		if r32 := rt.R32[i]; r32.Stride != 1 {
-			size := int((r32.Hi-r32.Lo)/r32.Stride) + 1
-			rt.R32 = append(rt.R32, make([]unicode.Range32, size-1)...)
-			copy(rt.R32[i+size:], rt.R32[i+1:])
-
-			for r := rune(r32.Lo); r <= rune(r32.Hi); r += rune(r32.Stride) {
-				rt.R32[i] = unicode.Range32{Lo: uint32(r), Hi: uint32(r), Stride: 1}
-				i++
-			}
-			i--
-		}
-	}
-
-	return rt
 }
