f26ce0bb946fd130a905fda3bc6d1453d3335362
Tom Thorogood <me+github@tomthorogood.co.uk>
2022-07-24 18:55:00 +0930

    cmd/passphrase: add passphrase generator command
    This command generates random passphrases using the EFF Large Wordlist
    for Passphrases (eff_large_wordlist.txt).

---
+++ cmd/passphrase/passphrase.go
@@ -0,0 +1,36 @@
+// Command passphrase generates random passphrases using the EFF Large Wordlist for
+// Passphrases (eff_large_wordlist.txt).
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"go.tmthrgd.dev/passit"
+)
+
+func init() {
+	flag.Usage = func() {
+		out := flag.CommandLine.Output()
+		fmt.Fprintln(out, "passphrase is a tool that generates random passphrases using the")
+		fmt.Fprintln(out, "EFF Large Wordlist for Passphrases (eff_large_wordlist.txt).")
+		fmt.Fprintln(out)
+		fmt.Fprintf(out, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	count := flag.Int("n", 6, "the number of words in the generated password")
+	flag.Parse()
+
+	pass, err := passit.EFFLargeWordlist(*count).Password(rand.Reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(pass)
+}
