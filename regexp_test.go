2334e4a9a80551fb6b163ecc76d20e5fb1dedc16
Tom Thorogood <me+github@tomthorogood.co.uk>
2022-10-02 12:56:54 +1030

    passit: remove allowedRangeTable reference in TestRegexpUnicodeAny

--- regexp_test.go
+++ regexp_test.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"regexp/syntax"
 	"testing"
+	"unicode"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -45,11 +46,53 @@ func TestRegexp(t *testing.T) {
 	}
 }
 
+// asciiGreek13RangeTable is rangeTableASCII + unicode.Greek @ 13.0.0.
+var asciiGreek13RangeTable = &unicode.RangeTable{
+	R16: []unicode.Range16{
+		{Lo: 0x0020, Hi: 0x007e, Stride: 1},
+		{Lo: 0x0370, Hi: 0x0373, Stride: 1},
+		{Lo: 0x0375, Hi: 0x0377, Stride: 1},
+		{Lo: 0x037a, Hi: 0x037d, Stride: 1},
+		{Lo: 0x037f, Hi: 0x0384, Stride: 5},
+		{Lo: 0x0386, Hi: 0x0388, Stride: 2},
+		{Lo: 0x0389, Hi: 0x038a, Stride: 1},
+		{Lo: 0x038c, Hi: 0x038e, Stride: 2},
+		{Lo: 0x038f, Hi: 0x03a1, Stride: 1},
+		{Lo: 0x03a3, Hi: 0x03e1, Stride: 1},
+		{Lo: 0x03f0, Hi: 0x03ff, Stride: 1},
+		{Lo: 0x1d26, Hi: 0x1d2a, Stride: 1},
+		{Lo: 0x1d5d, Hi: 0x1d61, Stride: 1},
+		{Lo: 0x1d66, Hi: 0x1d6a, Stride: 1},
+		{Lo: 0x1dbf, Hi: 0x1f00, Stride: 321},
+		{Lo: 0x1f01, Hi: 0x1f15, Stride: 1},
+		{Lo: 0x1f18, Hi: 0x1f1d, Stride: 1},
+		{Lo: 0x1f20, Hi: 0x1f45, Stride: 1},
+		{Lo: 0x1f48, Hi: 0x1f4d, Stride: 1},
+		{Lo: 0x1f50, Hi: 0x1f57, Stride: 1},
+		{Lo: 0x1f59, Hi: 0x1f5f, Stride: 2},
+		{Lo: 0x1f60, Hi: 0x1f7d, Stride: 1},
+		{Lo: 0x1f80, Hi: 0x1fb4, Stride: 1},
+		{Lo: 0x1fb6, Hi: 0x1fc4, Stride: 1},
+		{Lo: 0x1fc6, Hi: 0x1fd3, Stride: 1},
+		{Lo: 0x1fd6, Hi: 0x1fdb, Stride: 1},
+		{Lo: 0x1fdd, Hi: 0x1fef, Stride: 1},
+		{Lo: 0x1ff2, Hi: 0x1ff4, Stride: 1},
+		{Lo: 0x1ff6, Hi: 0x1ffe, Stride: 1},
+		{Lo: 0x2126, Hi: 0xab65, Stride: 35391},
+	},
+	R32: []unicode.Range32{
+		{Lo: 0x10140, Hi: 0x1018e, Stride: 1},
+		{Lo: 0x101a0, Hi: 0x1d200, Stride: 53344},
+		{Lo: 0x1d201, Hi: 0x1d245, Stride: 1},
+	},
+	LatinOffset: 1,
+}
+
 func TestRegexpUnicodeAny(t *testing.T) {
 	pattern := `a[bc]d[0-9][^\x00-AZ-az-\x{10FFFF}]a*b+c{4}d{3,6}e{5,}f?(g+h+)?.{2}[^a-z]+|x[0-9]+?.{0,5}(?:yy|zz)+(?P<punct>[[:punct:]])`
 
 	var p RegexpParser
-	p.SetAnyRangeTable(allowedRangeTable)
+	p.SetAnyRangeTable(asciiGreek13RangeTable)
 
 	tmpl, err := p.Parse(pattern, syntax.Perl)
 	require.NoError(t, err)
@@ -57,18 +100,18 @@ func TestRegexpUnicodeAny(t *testing.T) {
 	testRand := rand.New(rand.NewSource(0))
 
 	for _, expect := range []string{
-		"x90822236719\U00017e98\uaade\U000245df\ufe14yyyyzzzzyy=",
+		"x90822236719\u1f8cX\u03c4\u1f63yyyyzzzzyy=",
 		"x7977150zzyyyyzzzzzzyyzzzzyyyy<",
-		"x14404\uc5a2\U00022a0e\U0002102e\u8a35yyyyzzyyyyyyyyyyyyyyzz#",
-		"acd0saaaaaabbbbbbbbbbbbbbbbccccdddddeeeeeeeeeeeeeeeeee\U00021028\U0002d791\uc206\U0001b1dc\U000209bc\U000259fb\U00011113\U000243b9\u6d15\U00024d65\U0002106a\U0002a80b\U0001b230\u6664\U00025ce7\u2db9\U00020c6e",
-		"acd2haaaaaaabbbbbbbccccdddddeeeeeeeeeeeeeeeee\U0001bc5f\u674b\U0002b17e\U0002ac44\U0002297a\uac5a\U000283b2\U0002db0e\U00017100\u49f6\U000217b9\U000293b7\U0002cc55\uca25\U00025270\U0002ab5d\U00025d5c",
-		"acd3labbbbbbbbbbccccddddddeeeeeef\u59b8\U0002076a\U0002b5d2\U000265cd\u3780\U00023e1a\U0002d85e\U000294e1\u630b\U00027c99\u8ed2\ub80d\U0001d74b\u3c3b\U000211a9\U0002dd7a\U00023aaf",
-		"abd4iaaaaaaaabccccddddddeeeeeeeeeeeeeef\u38f7\u3fca\u9e41\u7432",
-		"x18372266066416\U0002ea3e\U00018a29yyyy?",
-		"acd2saaaaaaaaaaaaaabbbbbbbbbbbbbbbccccddddddeeeeeeeeeeeeeeeeegggggghhhhhh\U00027b8d\u37c8\U0002acaa\U0002bef5\U00028739\u24db\ufb56\U0002dbdb\U0001d093\U00022972\U00023f66\ud7d9\U00029ac1\u4ef2\u4e29\u6f67\U000211e4\U00026269",
-		"abd3Paaaaaabbbbbbbbbccccddddddeeeeeeeeeeeeeeef\U00023859\U00010f41\u4c52\U00029f1c\U0002d219\U0001682f\U0002b6a8\U00029eab\U000223d7",
-		"acd8Kaabbbbbbccccddddddeeeeee\U00023fa1\u132e\U0002a79e\u3625\U00027281\U00026d0c\U00014561\U0001340b\u8219\ua4e8\u539b\U00024f60\u9cdc\U00027efa\U00021d20\u47d2\ud43b",
-		"abd8sbbbbbbbccccddddeeeeeeeeef\U0002716a\U0002c5d7\u9884\u7fcf\uffc4\u3369\U0002b838\ufb8c\u0c1d\U0001d36f",
+		"x14404ZbS\u1f94yyyyzzyyyyyyyyyyyyyyzz#",
+		"acd0saaaaaabbbbbbbbbbbbbbbbccccdddddeeeeeeeeeeeeeeeeee\u1f75\u1f24\u03d7\U0001d23b\u1f78\u03ddPI\u03c9\u1fb2\u03a8\U00010173\U0001018d\u1fce\u03d1\u1f1a\u1fe8",
+		"acd2haaaaaaabbbbbbbccccdddddeeeeeeeeeeeeeeeee\u0398L3\u1fa0\U00010153\u0370\u1f2f\U00010159\u1d67\u1fcd\u1d5f\U00010160\U0001017b\u03f5\U00010188\U0001d22c_",
+		"acd3labbbbbbbbbbccccddddddeeeeeef\U0001016e\u1f31&\u1d61\u1fa1I\U000101765\u1fd7\u1f18\u03d2\u1f59\U0001d21b\U00010145\u1f6e\u1faf\U00010161",
+		"abd4iaaaaaaaabccccddddddeeeeeeeeeeeeeefm\u1fd7\u1f82\u1f2a",
+		"x18372266066416\U0001d21b\U0001018eyyyy?",
+		"acd2saaaaaaaaaaaaaabbbbbbbbbbbbbbbccccddddddeeeeeeeeeeeeeeeeegggggghhhhhh\u1f86\U00010161\U0001d203\U0001d205*V'\u1f66\u03ce\u1d27\u1f7c\u1f22\u03cbJ\u1fee\u1fd3SR",
+		"abd3Paaaaaabbbbbbbbbccccddddddeeeeeeeeeeeeeeef\u1f57\u1f4a)\u1fac\u03fe\u1f41\u1f26\u1fbdQ",
+		"acd8Kaabbbbbbccccddddddeeeeee\U0001d23e\u1f20\U0001d206\u1fcb\u1f9d\u1fab\u1f0c\u1ffd\u03d1\u03c5\U0001d209\u0386\U00010141\U0001017e\u03d0\u1fb8\u1f4b",
+		"abd8sbbbbbbbccccddddeeeeeeeeef@\u1f1c\U00010175\U00010166\u03c5\U0001d23e\U00010157GX\u03b2",
 		"x49868zzzzzzyyyyyyzzzzyyzzyyzz{",
 	} {
 		pass, err := tmpl.Password(testRand)
@@ -81,7 +124,7 @@ func TestRegexpUnicodeAny(t *testing.T) {
 		matchPattern := "^(?:" + pattern + ")$"
 		assert.Truef(t, regexp.MustCompile(matchPattern).MatchString(pass),
 			"regexp.MustCompile(%q).MatchString(%+q)", matchPattern, pass)
-		allRunesAllowed(t, allowedRangeTable, pass)
+		allRunesAllowed(t, asciiGreek13RangeTable, pass)
 	}
 }
 
