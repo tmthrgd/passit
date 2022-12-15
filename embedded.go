41252af45df51dd1d25833110569723cbeedb8ab
Tom Thorogood <me+github@tomthorogood.co.uk>
2022-12-15 19:35:52 +1030

    passit: enable pruning of unused embedded strings
    Writing a test for this is possible (see go.dev/cl/402555) but
    non-trivial. In this case I've tested this manually and confirmed that
    importing go.tmthrgd.dev/passit doesn't automatically pull in all the
    embedded strings.
    
    This is important as the four embedded strings currently consume
    114.5 kB. A number that is likely to increase especially as future
    Unicode versions add new emoji.

--- embedded.go
+++ embedded.go
@@ -11,13 +11,18 @@ import (
 
 type embeddedGenerator struct {
 	once sync.Once
-	raw  string
+
+	// Using a pointer to the embedded string allows for dead-code elimination
+	// to completely eliminate the embedded string if the Generator variable is
+	// never referenced.
+	raw *string
+
 	list []string
 }
 
 func (eg *embeddedGenerator) Password(r io.Reader) (string, error) {
 	eg.once.Do(func() {
-		eg.list = strings.Split(eg.raw, "\n")
+		eg.list = strings.Split(*eg.raw, "\n")
 	})
 	return readSliceN(r, eg.list)
 }
@@ -59,16 +64,16 @@ var (
 
 // EFFLargeWordlist is a Generator that returns a random word from the
 // EFF Large Wordlist for Passphrases (eff_large_wordlist.txt).
-var EFFLargeWordlist Generator = &embeddedGenerator{raw: effLargeWordlist}
+var EFFLargeWordlist Generator = &embeddedGenerator{raw: &effLargeWordlist}
 
 // EFFShortWordlist1 is a Generator that returns a random word from the
 // EFF Short Wordlist for Passphrases #1 (eff_short_wordlist_1.txt).
-var EFFShortWordlist1 Generator = &embeddedGenerator{raw: effShortWordlist1}
+var EFFShortWordlist1 Generator = &embeddedGenerator{raw: &effShortWordlist1}
 
 // EFFShortWordlist2 is a Generator that returns a random word from the
 // EFF Short Wordlist for Passphrases #2 (eff_short_wordlist_2_0.txt).
-var EFFShortWordlist2 Generator = &embeddedGenerator{raw: effShortWordlist2}
+var EFFShortWordlist2 Generator = &embeddedGenerator{raw: &effShortWordlist2}
 
 // Emoji13 is a Generator that returns a random emoji from the Unicode 13.0 emoji
 // list.
-var Emoji13 Generator = &embeddedGenerator{raw: emoji13List}
+var Emoji13 Generator = &embeddedGenerator{raw: &emoji13List}
