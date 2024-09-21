3da1a78b5f69e33d19d19d41a2b31dbd04f5988a
Tom Thorogood <me+github@tomthorogood.net>
2024-09-21 13:34:27 +0930

    internal/emojilist: expand TestEmojiCounts to cover Unicode15
    This was missed in 187fb54812.

--- internal/emojilist/embed_test.go
+++ internal/emojilist/embed_test.go
@@ -10,4 +10,5 @@ import (
 func TestEmojiCounts(t *testing.T) {
 	// Expected count is taken from https://www.unicode.org/Public/emoji/N.M/emoji-test.txt.
 	assert.Equal(t, 3295, strings.Count(Unicode13, "\n")+1, "Unicode 13.0")
+	assert.Equal(t, 3655, strings.Count(Unicode15, "\n")+1, "Unicode 15.0")
 }
